0c417c3dd0c2896ed7a7676c51409b2b3a5090e8
Felipe Diaz <fel.di.rod@gmail.com>
2025-08-12 15:35:25 -0400

    first commit

---
+++ src/test/java/cl/demo/camel/procesos/camel_demo/CamelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.demo.camel.procesos.camel_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
